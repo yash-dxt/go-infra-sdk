e44ec7068f24b4ab1d10e200925a6751229a30ee
Yash Dixit <yashdixitsq@gmail.com>
2023-02-17 18:28:47 +0530

    [test] add tests for utils, s3 & lambda

---
+++ lambda/invoke_test.go
@@ -0,0 +1,28 @@
+package lambda_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/metaphi-org/go-infra-sdk/config"
+	"github.com/metaphi-org/go-infra-sdk/lambda"
+	"github.com/stretchr/testify/assert"
+)
+
+type HelloWorld struct {
+	Msg string `json:"msg"`
+}
+
+var REGION = os.Getenv("AWS_REGION")
+var LAMBDA_NAME = os.Getenv("TEST_LAMBDA")
+
+func TestInvokeLambda(t *testing.T) {
+	res, err := lambda.InvokeLambda[HelloWorld](context.Background(), config.CreateAWSConfig(REGION), LAMBDA_NAME, lambda.RequestParams{
+		Endpoint: "/",
+		Method:   "GET",
+	})
+
+	assert.Nil(t, err)
+	assert.Equal(t, res.Msg, "helloworld")
+}
