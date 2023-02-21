521fb5406d22387a578ffde34a4b7cb949450ea8
Yash Dixit <yashdixitsq@gmail.com>
2023-02-21 17:01:10 +0530

    [fix] handle limitation of 1 req/s by time.sleep

--- ses/send_bulk_email.go
+++ ses/send_bulk_email.go
@@ -2,6 +2,7 @@ package email
 
 import (
 	"context"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/sesv2"
@@ -10,11 +11,17 @@ import (
 )
 
 // the 'from' email should be authorised in SES for the credentials in config.
+// this is to be used as a template email - each bulk Email entry corresponds to one email.
 func SendBulkEmail(config aws.Config, ctx context.Context, fromEmail string, defaultContent types.BulkEmailContent, bulkEmailEntries []types.BulkEmailEntry) error {
 
 	chunks := helpers.ArrayChunk(bulkEmailEntries, 50)
 
-	for _, chunk := range chunks {
+	for idx, chunk := range chunks {
+
+		if idx != 0 {
+			time.Sleep(2 * time.Second)
+		}
+
 		_, err := sesv2.NewFromConfig(config).SendBulkEmail(ctx, &sesv2.SendBulkEmailInput{
 			FromEmailAddress: &fromEmail,
 			DefaultContent:   &defaultContent,
